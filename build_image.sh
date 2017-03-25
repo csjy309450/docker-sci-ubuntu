cfe4475f5b96cabb040358db768476b8485c431c
Prototype <263693992@qq.com>
2017-03-26 01:39:31 +0800

    build shell

--- build_image.sh
+++
@@ -1,7 +0,0 @@
-image_name=test
-container_name=ctest
-password='123'
-
-echo ${password}|sudo -S docker build --rm -t ${image_name} .
-xhost +
-echo ${password}|sudo -S docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/yangzheng/test:/home/myShare --name=${container_name} --hostname=${container_name} ${image_name}
